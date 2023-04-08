fc575b53fea07256425485c8de9d8335cfb7c981
siferga <silvia.fernandez.pro@gmail.com>
2023-04-08 15:18:18 +0200

    hashPassword

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,10 @@ class UserType extends AbstractType
         $builder
             ->add('email')
             //->add('roles')
-            ->add('password')
+            //password do not 
+            ->add('plainPassword', TextType::class, [
+                'mapped' => false
+            ])
             ->add('isVerified');
     }
 
