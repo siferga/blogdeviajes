0ffce0f81f268924ca7f0f3c08dde13eeec70741
siferga <silvia.fernandez.pro@gmail.com>
2023-04-12 10:26:48 +0200

    CRUD profile update

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -8,71 +8,44 @@ use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/profile')]
 class ProfileController extends AbstractController
 {
-    #[Route('/', name: 'app_profile_index', methods: ['GET'])]
-    public function index(UserRepository $userRepository): Response
+
+    #[Route('', name: 'app_profile_show', methods: ['GET'])]
+    public function show(): Response
     {
-        return $this->render('profile/index.html.twig', [
-            'users' => $userRepository->findAll(),
-        ]);
-    }
-
-    #[Route('/new', name: 'app_profile_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, UserRepository $userRepository): Response
-    {
-        $user = new User();
-        $form = $this->createForm(User1Type::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $userRepository->save($user, true);
-
-            return $this->redirectToRoute('app_profile_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('profile/new.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_profile_show', methods: ['GET'])]
-    public function show(User $user): Response
-    {
+        $user = $this->getUser();
         return $this->render('profile/show.html.twig', [
             'user' => $user,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, UserRepository $userRepository): Response
+    #[Route('/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $userPasswordHasher): Response
     {
+        $user = $this->getUser();
         $form = $this->createForm(User1Type::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            //hashage password
+            $plainPassword = $form->get('plainPassword')->getData();
+            //password recup
+            $hashPassword = $userPasswordHasher->hashPassword($user, $plainPassword);
+            //remise de password in the user
+            $user->setPassword($hashPassword);
             $userRepository->save($user, true);
 
-            return $this->redirectToRoute('app_profile_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_profile_show', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('profile/edit.html.twig', [
+        return $this->render('profile/edit.html.twig', [
             'user' => $user,
             'form' => $form,
         ]);
-    }
-
-    #[Route('/{id}', name: 'app_profile_delete', methods: ['POST'])]
-    public function delete(Request $request, User $user, UserRepository $userRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
-            $userRepository->remove($user, true);
-        }
-
-        return $this->redirectToRoute('app_profile_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Form/User1Type.php
+++ src/Form/User1Type.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,10 +14,9 @@ class User1Type extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('isVerified')
-        ;
+            ->add('plainPassword', TextType::class, [
+                'mapped' => false
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
